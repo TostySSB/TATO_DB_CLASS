0b44c092d3930520d61f2806eb11d14c3ffd67c7
Troy Oster <toster1011@gmail.com>
2019-11-11 21:57:44 -0700

    Added inserts

--- tato_db_script.sql
+++ tato_db_script.sql
@@ -79,13 +79,67 @@ CREATE TABLE INVENTORY_ITEM(
     Item_name VARCHAR(100) NOT NULL,
     Item_description VARCHAR(255),
     Item_purchase_link VARCHAR(255),
-    Item_quantity int NOT NULL DEFAULT 0
+    Item_quantity int NOT NULL DEFAULT 0,
     PRIMARY KEY (Item_id)
-)
+);
 CREATE TABLE HAS_MERCH(
     Item_id int NOT NULL,
     Org_id int NOT NULL,
     PRIMARY KEY (Item_id),
     FOREIGN KEY (Item_id) REFERENCES INVENTORY_ITEM(Item_id),
     FOREIGN KEY (Org_id) REFERENCES ORGANIZATION(Org_id)
-)
+);
+
+insert into ORGANIZATION values  (1,"A club at MSU","Smash Society");
+insert into ORGANIZATION values  (2,"A club at UMN","Smash Central");
+insert into ORGANIZATION values  (3,"A club at UND","UND Smash");
+insert into ORGANIZATION values  (4,"A club at UCB","CO Smash");
+insert into ORGANIZATION values  (5,"A club at MSUB","Billings Smash Society");
+
+insert into USERS values (1,"toster1011@gmail.com", "Montana", "TostySSB",1);
+insert into USERS values (2,"johndoe@gmail.com", "Montana", "JDSSB",2);
+insert into USERS values (3,"DannyTrejo@gmail.com", "Montana", "DTSSB",3);
+insert into USERS values (4,"JohnnyDepp@gmail.com", "Montana", "JPSSB",4);
+insert into USERS values (5,"DannyD@gmail.com", "Montana", "DDSSB",5);
+
+insert into PLAYER values (1,"Tosty");
+insert into PLAYER values (2,"JD");
+insert into PLAYER values (3,"DT");
+insert into PLAYER values (4,"JP");
+insert into PLAYER values (5,"DD");
+
+insert into OWNED_BY values (1,1);
+insert into OWNED_BY values (2,2);
+insert into OWNED_BY values (3,3);
+insert into OWNED_BY values (4,4);
+insert into OWNED_BY values (5,5);
+
+insert into HAS_MEMBER values (1,1);
+insert into HAS_MEMBER values (2,2);
+insert into HAS_MEMBER values (3,3);
+insert into HAS_MEMBER values (4,4);
+insert into HAS_MEMBER values (5,5);
+
+insert into TOURNAMENT values (1,"Montana",10);
+insert into TOURNAMENT values (2,"Montana",12);
+insert into TOURNAMENT values (3,"Montana",15);
+insert into TOURNAMENT values (4,"Montana",19);
+insert into TOURNAMENT values (5,"Montana",20);
+
+insert into PLAYS_IN values (1,1);
+insert into PLAYS_IN values (2,1);
+insert into PLAYS_IN values (3,1);
+insert into PLAYS_IN values (4,1);
+insert into PLAYS_IN values (5,1);
+
+insert into BRACKET values (1,1,"Ultimate Singles");
+insert into BRACKET values (2,1,"Melee Singles");
+insert into BRACKET values (3,2,"Ultimate Singles");
+insert into BRACKET values (4,3,"Ultimate Singles");
+insert into BRACKET values (5,4,"Melee Singles");
+
+insert into PLACINGS values (1,"1. Tosty 2. JD")
+insert into PLACINGS values (2,"1. Tosty 2. JD")
+insert into PLACINGS values (3,"1. Tosty 2. JD")
+insert into PLACINGS values (4,"1. Tosty 2. JD")
+insert into PLACINGS values (5,"1. Tosty 2. JD")
